5da63a61b9b81569d5b6fa895c4290941c8e26d4
Miroslav Bendík <miroslav.bendik@gmail.com>
2015-09-13 14:09:30 +0200

    Fixed missing page kwarg

--- django_simple_paginator/templatetags/paginator_tags.py
+++ django_simple_paginator/templatetags/paginator_tags.py
@@ -38,6 +38,7 @@ def pager_url(context, page_num):
 	resolver_match = context['resolver_match']
 	page_kwarg = context['page_kwarg']
 	kwargs = {k: v for k, v in resolver_match.kwargs.iteritems() if v is not None}
+	kwargs[page_kwarg] = page_num
 	try:
 		url_args = '?' + request.GET.urlencode() if request.GET else ''
 		full_url = reverse(resolver_match.view_name, args=resolver_match.args, kwargs=kwargs) + url_args
