1402adfb77954e0c080d0868ec4c1547170110c7
Miroslav Bendík <miroslav.bendik@gmail.com>
2018-02-08 09:01:14 +0100

    Added support for url_name parameter in pagination

--- django_simple_paginator/templatetags/paginator_tags.py
+++ django_simple_paginator/templatetags/paginator_tags.py
@@ -40,48 +40,62 @@ def assign_range_to_page_obj(page_obj, inner, outer):
 	return page_obj
 
 
-def pagination_ctx(context, page_obj, page_kwarg, inner, outer):
+def pagination_ctx(context, page_obj, page_kwarg, inner, outer, url_name, url_args, url_kwargs):
 	page_obj = page_obj or context['page_obj']
 	if hasattr(context, 'flatten'):
 		inner_context = context.flatten()
 	else:
 		inner_context = dict(context)
+	if url_name is None:
+		request = context['request']
+		resolver_match = request.resolver_match
+		url_name = resolver_match.view_name
+		url_args = resolver_match.args
+		url_kwargs = resolver_match.kwargs
+	else:
+		url_args = url_args or []
+		url_kwargs = url_kwargs or {}
 	ctx_update = {
 		'page_obj': assign_range_to_page_obj(page_obj, inner, outer),
 		'page_kwarg': page_kwarg,
+		'url_name': url_name,
+		'url_args': url_args,
+		'url_kwargs': url_kwargs,
 	}
 	inner_context.update(ctx_update)
 	return inner_context
 
 
 @register.simple_tag(takes_context=True)
-def pagination(context, page_obj=None, page_kwarg='page', template_name='paginator/paginator.html', inner=PAGINATOR_INNER_COUNT, outer=PAGINATOR_OUTER_COUNT): # pylint: disable=too-many-arguments
-	rendered = render_to_string(template_name, pagination_ctx(context, page_obj, page_kwarg, inner, outer))
+def pagination(context, page_obj=None, page_kwarg='page', template_name='paginator/paginator.html', inner=PAGINATOR_INNER_COUNT, outer=PAGINATOR_OUTER_COUNT, url_name=None, *url_args, **url_kwargs): # pylint: disable=too-many-arguments
+	rendered = render_to_string(template_name, pagination_ctx(context, page_obj, page_kwarg, inner, outer, url_name, url_args, url_kwargs))
 	return mark_safe(rendered)
 
 
 @register.simple_tag(takes_context=True)
 def pager_url(context, page_num):
 	request = context['request']
-	resolver_match = request.resolver_match
+	url_name = context['url_name']
+	url_args = context['url_args']
+	url_kwargs = context['url_kwargs']
 	page_kwarg = context['page_kwarg']
-	kwargs = {k: v for k, v in resolver_match.kwargs.items() if v is not None}
+	kwargs = {k: v for k, v in url_kwargs.items() if v is not None}
 	kwargs[page_kwarg] = page_num
 	try:
 		url_args = '?' + request.GET.urlencode() if request.GET else ''
-		full_url = reverse(resolver_match.view_name, args=resolver_match.args, kwargs=kwargs) + url_args
+		full_url = reverse(url_name, args=url_args, kwargs=kwargs) + url_args
 		if page_num == 1:
-			kwargs = resolver_match.kwargs.copy()
+			kwargs = url_kwargs.copy()
 			kwargs.pop(page_kwarg, None)
 			try:
-				return reverse(resolver_match.view_name, args=resolver_match.args, kwargs=kwargs) + url_args
+				return reverse(url_name, args=url_args, kwargs=kwargs) + url_args
 			except NoReverseMatch:
 				pass
 		return full_url
 	except NoReverseMatch:
 		get = request.GET.copy()
 		get[page_kwarg] = page_num
-		base_url = reverse(resolver_match.view_name, args=resolver_match.args, kwargs=resolver_match.kwargs)
+		base_url = reverse(url_name, args=url_args, kwargs=url_kwargs)
 		return base_url + '?' + get.urlencode()
 
 
