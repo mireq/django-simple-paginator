170f89534a9e295ec6399f0ea461d76860b781ea
Miroslav Bendík <miroslav.bendik@gmail.com>
2018-06-24 17:49:28 +0200

    Added page converter

---
+++ django_simple_paginator/converter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class PageConverter:
+	regex = r'(?:\d+/)?'
+
+	def to_python(self, value):
+		value = value.rstrip('/')
+		if value:
+			return int(value)
+		else:
+			return 1
+
+	def to_url(self, value):
+		if value == 1:
+			return ''
+		else:
+			return '%d/' % value
