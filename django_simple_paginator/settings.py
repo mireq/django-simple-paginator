59dc7cc6efaac87a4e682855be6b18a0571bc31c
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-17 06:03:31 +0100

    refactor: Renamed settings to match django get_elided_page_range

--- django_simple_paginator/settings.py
+++ django_simple_paginator/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-PAGINATOR_INNER_COUNT = getattr(settings, "PAGINATOR_INNER_COUNT", 3)
-PAGINATOR_OUTER_COUNT = getattr(settings, "PAGINATOR_OUTER_COUNT", 1)
+PAGINATOR_ON_EACH_SIDE = getattr(settings, "PAGINATOR_ON_EACH_SIDE", 3)
+PAGINATOR_ON_ENDS = getattr(settings, "PAGINATOR_ON_ENDS", 1)
 PAGINATOR_TEMPLATE_NAME = getattr(settings, "PAGINATOR_TEMPLATE_NAME", 'paginator/paginator.html')
